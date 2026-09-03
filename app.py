4febb691e6e270a8626a19b2f649afcf3b60d41b
MohidAhmed6115 <mohidahmed6115@gmail.com>
2026-09-03 17:19:41 +0500

    Added Quiz Route and fetching questions

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask,session,render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
+from sqlalchemy import Integer, String, Text, DateTime, func
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, EmailField, RadioField
 from wtforms.validators import InputRequired, Length, ValidationError
-from datetime import timedelta
+import math
+from datetime import timedelta, datetime
 load_dotenv()
 
 password = quote_plus(os.getenv('DB_PASSWORD'))
@@ -37,6 +38,25 @@ login_manager.login_view = 'login'
 def load_user(user_id):
 	return db.session.get(Users, int(user_id))
 
+class Questions(db.Model):
+	__tablename__ = "questions"
+
+	id:Mapped[int] = mapped_column(primary_key=True)
+	category: Mapped[str] = mapped_column(String(100))
+	question_text: Mapped[str] = mapped_column(Text)
+	option_a : Mapped[str] = mapped_column(Text)
+	option_b : Mapped[str] = mapped_column(Text)
+	option_c : Mapped[str] = mapped_column(Text)
+	option_d : Mapped[str] = mapped_column(Text)
+	correct_option : Mapped[str] = mapped_column(String(1))
+
+class QuizAttempts(db.Model):
+	__tablename__ = "quiz_attempts"
+	id:Mapped[int] = mapped_column(primary_key=True)
+	user_id: Mapped[int] = mapped_column(unique = True)
+	category: Mapped[str] = mapped_column(String(100))
+	score: Mapped[int] = mapped_column(nullable = True)
+	taken_at : Mapped[datetime] = mapped_column(DateTime,server_default = func.now())
 
 class Users(UserMixin,db.Model):
 	__tablename__ = "users"
@@ -77,6 +97,11 @@ class RegisterForm(FlaskForm):
 			raise ValidationError(
 				"Email already used."
 			)
+
+class QuizQuestionChoice(FlaskForm):
+	answer = RadioField('Choose an Answer', validators=[InputRequired()])
+
+	submit = SubmitField("Next") 
 
 @app.route("/")
 def home():
@@ -134,6 +159,53 @@ def dashboard():
 def categories():
 	return render_template("categories.html")
 
+@app.route('/quiz/<category>', methods = ["GET","POST"])
+@login_required
+def start_quiz(category):
+
+	questions = Questions.query.filter_by(category = category).all()
+	last = len(questions) - 1
+	page = request.args.get('page')
+	if not str(page).isnumeric():
+		page = 0
+	else:
+		page = int(page)
+
+	# Slicing of array questions
+	# [arg * no of question in a page : arg * no of question in a page + no of question in a page]
+	# Only One Question Per page
+	questions = questions[page*1:page*1+1]
+	current_question = questions[0] 
+	form = QuizQuestionChoice()
+	form.answer.choices = [
+		('a',current_question.option_a),
+		('b',current_question.option_b),
+		('c',current_question.option_c),
+		('d',current_question.option_d)
+	]
+	if page == 0 and page == last:
+		prev = "#"
+		next = "#"
+	elif page == 0:
+		prev = "#"
+		next = url_for('start_quiz',category = category) + "?page=" + str(page + 1)
+	elif page == last:
+		prev = url_for('start_quiz',category = category) + "?page=" + str(page - 1)
+		next = "#"
+	else:
+		prev = url_for('start_quiz',category = category) + "?page=" + str(page - 1)
+		next = url_for('start_quiz',category = category) + "?page=" + str(page + 1)
+	
+	if form.validate_on_submit():
+		selected_answer = form.answer.data
+		answers = session.get('quiz_answers',{})
+		answers[str(current_question.id)] = selected_answer
+		session['quiz_answers'] = answers
+				
+		if page == last:
+			return redirect(url_for('quiz_results'))
+		return redirect(next)
+	return render_template("quiz.html", question = current_question, prev = prev, next = next, form = form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
